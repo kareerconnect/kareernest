0f0071823edb75e064d66b204a3b2893065c9c06
surazkarn <surazkarn8848@gmail.com>
2024-02-22 10:54:56 +0530

    Login Page done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -18,14 +18,21 @@ const login = () => {
             </div>
 
             <div>
-            <label className='label'>
-                <span className='text-base label-text'> Password</span>
+              <label className='label'>
+                  <span className='text-base label-text'> Password</span>
               </label>
               <input 
                 type='password' 
                 placeholder='Enter Password' 
                 className='w-full input input-bordered h-10'
               />
+            </div>
+            <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+              {"Don't"} have an account?
+            </a>
+
+            <div>
+              <button className='btn btn-block btn-sm mt-2'>Login</button>
             </div>
           </form>
         </div>
