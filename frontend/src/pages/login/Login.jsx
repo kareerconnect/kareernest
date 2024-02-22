164661ee85495f976090a141345135f8f42a8cf5
surazkarn <surazkarn8848@gmail.com>
2024-02-22 11:21:56 +0530

    Login Page done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -12,22 +12,22 @@ const login = () => {
           <form>
             <div>
               <label className='label p-2'>
-                <span className='text-base label-text'>Username</span>
+                <span className='text-base label-text text-slate-300'>Email</span>
               </label>
-              <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+              <input type='text' placeholder='Enter your email' className='w-full input input-bordered h-10' />
             </div>
 
             <div>
               <label className='label'>
-                  <span className='text-base label-text'> Password</span>
+                  <span className='text-base label-text text-slate-300'> Password</span>
               </label>
               <input 
                 type='password' 
-                placeholder='Enter Password' 
+                placeholder='Enter your password' 
                 className='w-full input input-bordered h-10'
               />
             </div>
-            <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+            <a href='#' className='text-sm text-neutral-300 hover:underline hover:text-blue-600 mt-2 inline-block'>
               {"Don't"} have an account?
             </a>
 
