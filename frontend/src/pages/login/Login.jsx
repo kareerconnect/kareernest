bc05cb11a313580d0eb38798fb075e6d196808f2
surazkarn <surazkarn8848@gmail.com>
2024-02-22 16:33:12 +0530

    Signup Page done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -41,3 +41,48 @@ const login = () => {
 }
 
 export default login
+
+//Starter Code for this file:
+
+// const login = () => {
+//   return (
+//     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//           <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//             Login
+//             <span className='text-lime-300'> Alien</span>
+//           </h1>
+
+//           <form>
+//             <div>
+//               <label className='label p-2'>
+//                 <span className='text-base label-text text-slate-300'>Email</span>
+//               </label>
+//               <input type='text' placeholder='Enter your email' className='w-full input input-bordered h-10' />
+//             </div>
+
+//             <div>
+//               <label className='label'>
+//                   <span className='text-base label-text text-slate-300'> Password</span>
+//               </label>
+//               <input 
+//                 type='password' 
+//                 placeholder='Enter your password' 
+//                 className='w-full input input-bordered h-10'
+//               />
+//             </div>
+//             <a href='#' className='text-sm text-neutral-300 hover:underline hover:text-blue-600 mt-2 inline-block'>
+//               {"Don't"} have an account?
+//             </a>
+
+//             <div>
+//               <button className='btn btn-block btn-sm mt-2'>Login</button>
+//             </div>
+//           </form>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default login
+
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
-
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+          <h1 className='text-3xl font-semibold text-center text-gray-300'>
+            Sign Up
+            <span className='text-lime-300'> Alien</span>
+          </h1>
+
+          <form>
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text text-slate-300'>Email</span>
+              </label>
+              <input type='text' placeholder='Enter your email' className='w-full input input-bordered h-10' />
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text text-slate-300'>Username</span>
+              </label>
+              <input type='text' placeholder='Enter your username' className='w-full input input-bordered h-10' />
+            </div>
+
+            <div>
+              <label className='label'>
+                  <span className='text-base label-text text-slate-300'> Password</span>
+              </label>
+              <input 
+                type='password' 
+                placeholder='Enter your password' 
+                className='w-full input input-bordered h-10'
+              />
+            </div>
+            <a href='#' className='text-sm text-neutral-300 hover:underline hover:text-blue-600 mt-2 inline-block'>
+              Already have an account?
+            </a>
+
+            <div>
+              <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+            </div>
+          </form>
+        </div>
+    </div>
   )
 }
 
 export default SignUp
+
+
+//Starter code for SignUp component
+
+// const SignUp = () => {
+//   return (
+//     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//           <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//             Sign Up
+//             <span className='text-lime-300'> Alien</span>
+//           </h1>
+
+//           <form>
+//             <div>
+//               <label className='label p-2'>
+//                 <span className='text-base label-text text-slate-300'>Email</span>
+//               </label>
+//               <input type='text' placeholder='Enter your email' className='w-full input input-bordered h-10' />
+//             </div>
+
+//             <div>
+//               <label className='label p-2'>
+//                 <span className='text-base label-text text-slate-300'>Username</span>
+//               </label>
+//               <input type='text' placeholder='Enter your username' className='w-full input input-bordered h-10' />
+//             </div>
+
+//             <div>
+//               <label className='label'>
+//                   <span className='text-base label-text text-slate-300'> Password</span>
+//               </label>
+//               <input 
+//                 type='password' 
+//                 placeholder='Enter your password' 
+//                 className='w-full input input-bordered h-10'
+//               />
+//             </div>
+//             <a href='#' className='text-sm text-neutral-300 hover:underline hover:text-blue-600 mt-2 inline-block'>
+//               Already have an account?
+//             </a>
+
+//             <div>
+//               <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+//             </div>
+//           </form>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default SignUp
