1c60026d84e84b27a0b2bc730d7471f39282afc6
surazkarn <surazkarn8848@gmail.com>
2024-02-22 10:38:30 +0530

    Login Page frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+          <h1 className='text-3xl font-semibold text-center text-gray-300'>
+            Login
+            <span className='text-lime-300'> Alien</span>
+          </h1>
+
+          <form>
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Username</span>
+              </label>
+              <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+            </div>
+
+            <div>
+            <label className='label'>
+                <span className='text-base label-text'> Password</span>
+              </label>
+              <input 
+                type='password' 
+                placeholder='Enter Password' 
+                className='w-full input input-bordered h-10'
+              />
+            </div>
+          </form>
+        </div>
+    </div>
   )
 }
 
