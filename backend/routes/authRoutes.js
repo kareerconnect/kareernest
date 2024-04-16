5db746682f65c8af2ecedafc7faa9769356dcd91
surazkarn <surazkarn8848@gmail.com>
2024-04-17 01:58:28 +0530

    logout route and controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,5 +7,6 @@ router.post('/signup', authController.signup);
 router.post('/verify', authController.verify);
 router.post('/resend-otp', authController.resendOTP);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
